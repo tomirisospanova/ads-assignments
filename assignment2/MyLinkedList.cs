4836e921fccbacd00f2e9202677b5eb8035c076b
prenl <elnur.kazakhstan@gmail.com>
2023-04-12 16:54:31 +0600

    add: MyLinkedList & MyNode classes

---
+++ assignment2/MyLinkedList.cs
@@ -0,0 +1,18 @@
+﻿namespace assignment2;
+
+public class MyLinkedList<T>
+{
+
+    private class MyNode
+    {
+        private readonly T data;
+        private MyNode next;
+
+        public MyNode(T data)
+        {
+            this.data = data;
+        }
+    }
+
+
+}
