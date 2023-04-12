3d814deecb12cc284c64c5baf4e3ca541ebfb2f7
prenl <elnur.kazakhstan@gmail.com>
2023-04-12 15:11:21 +0600

    add(MyArrayList): constructors

--- assignment2/MyArrayList.cs
+++ assignment2/MyArrayList.cs
@@ -7,7 +7,13 @@ public class MyArrayList<T>
     private object[] _hiddenArr;
     private int _length;
 
-    
+    public MyArrayList() : this(5) {}
+
+    public MyArrayList(int initCapacity) 
+    {
+        _hiddenArr = new object[initCapacity];
+    }
+
 
 
     
