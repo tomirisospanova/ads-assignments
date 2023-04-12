ffa7e08e69c5972f6d2d00dce1c3af70b87bb0b7
prenl <elnur.kazakhstan@gmail.com>
2023-04-12 15:36:27 +0600

    refactor(MyArrayList): change object to T

--- assignment2/MyArrayList.cs
+++ assignment2/MyArrayList.cs
@@ -2,22 +2,21 @@
 
 public class MyArrayList<T> 
 {
-
     
-    private object[] _hiddenArr;
+    private T[] _hiddenArr;
     private int _length;
 
     public MyArrayList() : this(5) {}
 
     public MyArrayList(int initCapacity) 
     {
-        _hiddenArr = new object[initCapacity];
+        _hiddenArr = new T[initCapacity];
     }
 
     private void IncreaseArray() 
     {
         int newSize = (int)(_hiddenArr.Length * 1.5);
-        object[] newArr = new object[newSize];
+        T[] newArr = new T[newSize];
 
         for (int i = 0; i < _hiddenArr.Length; i++) 
         {
@@ -27,7 +26,7 @@ public class MyArrayList<T>
         _hiddenArr = newArr;
     }
     
-    public void Add(object item)
+    public void Add(T item)
     {
         if (_length == _hiddenArr.Length)
         {
@@ -36,18 +35,18 @@ public class MyArrayList<T>
 
         _hiddenArr[_length++] = item;
     }
-    
+
     public T Get(int index)
     {
         if (index >= _hiddenArr.Length) 
             throw new IndexOutOfRangeException();
 
-        return (T)_hiddenArr[index];
+        return _hiddenArr[index];
     }
 
     public int Size()
     {
         return _length;
     }
-    
+
 }
