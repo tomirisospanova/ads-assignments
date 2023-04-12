03405da273241c951c9f13a45e308630135f9f3a
prenl <elnur.kazakhstan@gmail.com>
2023-04-12 17:05:13 +0600

    add(MyLinkedList): Add method

--- assignment2/MyLinkedList.cs
+++ assignment2/MyLinkedList.cs
@@ -5,8 +5,9 @@ public class MyLinkedList<T>
 
     private class MyNode
     {
-        private readonly T data;
-        private MyNode next;
+        public readonly T data;
+        public MyNode prev;
+        public MyNode next;
 
         public MyNode(T data)
         {
@@ -14,5 +15,29 @@ public class MyLinkedList<T>
         }
     }
 
+    private MyNode head;
+    private int length;
 
+    public void Add(T item)
+    {
+        MyNode newNode = new MyNode(item);
+        length++;
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+
+        MyNode temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        temp.next.prev = temp;
+    }
+
+    
 }
+
